e5842a5ceed4a1cb6ccf03693ccec7c6c4415798
Lukas Georgieff <lukas.georgieff@hotmail.com>
2018-10-02 08:28:29 -0700

    src: Change single argument constructors to explicit

--- cpp/src/bmw_manufacturer.hpp
+++ cpp/src/bmw_manufacturer.hpp
@@ -5,7 +5,7 @@
 namespace testing_sample {
 class bmw_manufacturer : public car_manufacturer {
  public:
-  bmw_manufacturer(const vin_pool &vin_pool);
+  explicit bmw_manufacturer(const vin_pool &vin_pool);
   bmw_manufacturer(const bmw_manufacturer &) = default;
   bmw_manufacturer(bmw_manufacturer &&) noexcept  = default;
   bmw_manufacturer &operator=(const bmw_manufacturer &) = default;
--- cpp/src/engine.hpp
+++ cpp/src/engine.hpp
@@ -4,7 +4,7 @@
 namespace testing_sample {
 class engine {
  public:
-  engine(unsigned short horse_power) noexcept;
+  explicit engine(unsigned short horse_power) noexcept;
   engine(const engine &) = default;
   engine(engine &&) = default;
   engine &operator=(const engine &) = default;
